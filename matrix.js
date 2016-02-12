2b7c8b2c190507b06557dd5d427d162c67d4c19a
Luis <luis@netbeast.co>
2016-02-12 13:44:23 +0100

    Refactoring

---
+++ matrix.js
@@ -0,0 +1,34 @@
+var	helper = require('../src/helper.js')
+var async = require('async')
+
+var beecolor = module.exports = {}
+
+beecolor.matrix = function (matrix, callback) {
+	async.series([
+		async.apply(helper.register),
+		//Function to be executed here
+		async.apply(helper.matrix, matrix)
+	], function(err, results) { 
+		if(typeof callback !== 'function') throw err
+		callback(err)
+	})
+} 
+
+ 
+beecolor.printPixel = function (x, y, callback) {
+	async.series([
+		async.apply(helper.register),
+		async.apply(printPixel(x,y))
+	], function(err, results) { 
+		if(typeof callback !== 'function') throw err
+		callback(err) 
+	})
+}
+
+beecolor.clear = function (callback) {
+	helper.clear(function(err) {
+		if(typeof callback !== 'function') throw err
+		callback(err)
+	})
+}
+ 
